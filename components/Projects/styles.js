64c7c4e3f3ea4252de6510f7c8ccd9fa897e58bd
mooenz <joss92821@hotmail.com>
2022-03-25 10:26:01 -0500

    cambio de la distribucion de los proyectos

--- components/Projects/styles.js
+++ components/Projects/styles.js
@@ -10,12 +10,12 @@ export const Projects = styled.div`
 
   @media screen and (min-width: 790px) {
     display: grid;
-    grid-template-columns: repeat(2, auto);
+    grid-template-columns: repeat(2, 1fr);
     place-content: center;
   }
 
   @media screen and (min-width: 1200px) {
-    grid-template-columns: repeat(3, auto);
+    grid-template-columns: repeat(3, 1fr);
     gap: 30px;
   }
 `;
