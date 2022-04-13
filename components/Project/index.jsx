b1ef9f0e7bcc7ed7e59f39e1f758030346568428
mooenz <joss92821@hotmail.com>
2022-04-12 21:32:22 -0500

    nuevo estilo

--- components/Project/index.jsx
+++ components/Project/index.jsx
@@ -1,27 +1,17 @@
 //styles
 
-import { Article, Container, Img, Tag, Title, Anchor } from './styles';
+import { Article, Tag } from './styles';
 
 const Project = ({ item }) => {
   const { title, tag, repository, image, live } = item;
 
   return (
-    <Article>
-      <Img src={image} alt={title} width={300} height={200} />
-      <Container>
-        <Tag>
-          <span>{tag}</span>
-        </Tag>
-        <div>
-          <a href={live} target="_blank" rel="noopener noreferrer">
-            <Title>{title}.</Title>
-          </a>
-          <Anchor href={repository} target="_blank" rel="noopener noreferrer">
-            View code
-          </Anchor>
-        </div>
-      </Container>
-    </Article>
+    <a href={live} target="_blank" rel="noopener noreferrer">
+      <Article title={title} image={image}>
+        <Tag>{tag}</Tag>
+        <h2>{title}</h2>
+      </Article>
+    </a>
   );
 };
 
--- components/Project/styles.js
+++ components/Project/styles.js
@@ -2,87 +2,63 @@ import styled from 'styled-components';
 import Theme from '../../constants/Theme';
 
 // Colors
-const { beige, purple, green, red } = Theme;
+const { beige, purple, green } = Theme;
 
 export const Article = styled.article`
   display: flex;
   width: 100%;
   height: 250px;
-  flex-direction: column;
+  align-items: flex-end;
   border-radius: 0.625rem;
   max-width: 378px;
   margin: 0 auto;
   position: relative;
-  border: 1px solid transparent;
-  transition-duration: 0.5s;
-
+  background: ${({ image }) => `url("${image}")  center/cover no-repeat`};
 
   &:hover {
-    border: 1px solid ${green};
-    border-radius: 11px;
+    h2 {
+      opacity: 1;
+    }
   }
-`;
 
-export const Img = styled.img`
-  width: 100%;
-  min-height: 100%;
-  object-fit: cover;
-  object-position: top;
-  border-radius: 10px;
-  transition-duration: 0.5s;
-
-  &:hover {
-    opacity: 0.9;
+  h2 {
+    font-size: 2.5rem;
+    font-weight: 700;
+    color: ${beige};
+    z-index: 1;
+    margin: 20px;
+    opacity: 0;
   }
-`;
 
-export const Container = styled.section`
-  background: linear-gradient(180deg, rgba(0, 0, 0, 0) 20%, ${purple} 95%);
-  position: absolute;
-  border-radius: 10px;
-  color: ${beige};
-  padding: 24px;
-  width: 100%;
-  height: 100%;
-  font-size: 1.5rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  transition: all 0.5s;
+  &::after {
+    content: '';
+    background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, ${purple} 100%);
+    border-radius: 0.625rem;
+    height: 0;
+    bottom: 0;
+    left: 0;
+    position: absolute;
+    transition-duration: 0.4s ease-in;
+    width: 100%;
+  }
+
+  &:hover::after {
+    height: 100%;
+  }
 `;
 
 export const Tag = styled.p`
   color: ${purple};
+  font-size: 12px;
   font-weight: 700;
   letter-spacing: 1.5px;
-  margin-bottom: 10px;
   text-transform: uppercase;
-  text-align: end;
-
-  span {
-    background-color: ${green};
-    padding: 8px;
-    border-radius: 10px;
-  }
-`;
-
-export const Title = styled.h2`
-  font-size: 2.5rem;
-  margin-bottom: 12px;
-
-  &:hover {
-    text-decoration: underline;
-  }
-`;
-
-export const Anchor = styled.a`
-  text-decoration: underline;
-  text-underline-offset: 2px;
-  transition-duration: 0.5s;
-  color: ${green};
-  width: 70px;
-
-  &:hover {
-    color: ${red};
-  }
+  text-align: center;
+  background-color: ${green};
+  padding: 8px;
+  border-radius: 10px;
+  width: 80px;
+  position: absolute;
+  top: 20px;
+  right: 20px;
 `;
